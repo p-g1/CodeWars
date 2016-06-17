63ca80b213160d707b2482cad0cae0b6473b4e30
Pete <petergarvin1@gmail.com>
2016-06-17 20:41:08 +0100

    first entry

---
+++ 8kyu_Drink_about.js
@@ -0,0 +1,14 @@
+var peopleWithAgeDrink = function(old) {
+  if(old<14){
+    return "drink toddy";
+  }
+  else if(old<18){
+    return "drink coke";
+  }
+  else if(old<21){
+    return "drink beer";
+  }
+  else {
+    return "drink whisky";
+  }
+};
