08ce16f4d34d7c6d6444a604b2f3a5e830bb2846
Pete <petergarvin1@gmail.com>
2016-06-23 22:22:20 +0100

    first entry

---
+++ 8kyu_Jennys_secret_message.js
@@ -0,0 +1,8 @@
+//Challenge: https://www.codewars.com/kata/55225023e1be1ec8bc000390
+
+function greet(name){
+  if(name === "Johnny")
+    return "Hello, my love!";
+  else
+    return "Hello, " + name + "!";
+}
