1573d7f33a36cc7a0226b693b82757fbc923b6dd
Pete <petergarvin1@gmail.com>
2016-06-26 12:38:54 +0100

    first entry

---
+++ 8kyu_Convert_boolean_vaues_to_strings_Yes_or_No.js
@@ -0,0 +1,12 @@
+//Challenge: https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+rorysedgwick, anniva, asbren13, zezo, madziaalma, Capri-Tsun (plus 12 more warriors)
+
+function boolToWord( bool ){
+  if (bool === true) {
+    return "Yes";
+  }
+  else if (bool === false) {
+    return "No";
+  }
+}
