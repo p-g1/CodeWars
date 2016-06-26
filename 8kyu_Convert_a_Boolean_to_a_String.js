f8a28d9e32eaf4679a7425aa8577458b6a183123
Pete <petergarvin1@gmail.com>
2016-06-26 12:36:30 +0100

    first entry

---
+++ 8kyu_Convert_a_Boolean_to_a_String.js
@@ -0,0 +1,9 @@
+//Challenge: https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+function booleanToString(b){
+  if(b === true) {
+    return "true";
+  } else if (b === false) {
+    return "false";
+  }
+}
