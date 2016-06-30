261ddeccf5cb1c9fc540317f18b3932b86faa2bd
Pete <petergarvin1@gmail.com>
2016-06-30 20:17:12 +0100

    first entry

---
+++ 8kyu_Switch_Case_Bug_Fixing_6.js
@@ -0,0 +1,20 @@
+//Challenge: https://www.codewars.com/kata/55c933c115a8c426ac000082
+
+function evalObject(value){
+  var result = 0;
+  switch(value.operation){
+    case'+': result = value.a + value.b;
+      break;
+    case'-': result = value.a - value.b;
+      break;
+    case'/': result = value.a / value.b;
+      break;
+    case'*': result = value.a * value.b;
+      break;
+    case'%': result = value.a % value.b;
+      break;
+    case'^': result = Math.pow(value.a, value.b);
+      break;
+  }
+  return result;
+}
