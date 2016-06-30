d41b61bef9d75970d09198e3d400bef7e1ebf60d
Pete <petergarvin1@gmail.com>
2016-06-30 20:14:49 +0100

    first entry

---
+++ 8kyu_Grasshopper_Shopping_list.js
@@ -0,0 +1,8 @@
+//Challenge: https://www.codewars.com/kata/560c31275c39c481c4000022
+
+var sandwiches = 4;
+var salads= 6;
+var wraps= 5;
+var frenchFries= 10;
+
+var totalPrice= sandwiches * 8.00 + salads * 7.00 + wraps * 6.50 + frenchFries * 1.20
