f26d4c25f3e4360415cfb202b96fc2006e4d6740
Pete <petergarvin1@gmail.com>
2016-07-03 21:45:37 +0100

    first entry

---
+++ 8kyu_Is_it_even.js
@@ -0,0 +1,10 @@
+//Challenge: https://www.codewars.com/kata/555a67db74814aa4ee0001b5
+
+function testEven(n) {
+    if (n % 1 !== 0 || n % 2 !== 0) {
+      return false;
+      }
+    else {
+      return true;
+      }
+}
