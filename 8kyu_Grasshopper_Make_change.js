58a5d013d7a07bbb6f4a58dbd5e37d5a590eb39b
Pete <petergarvin1@gmail.com>
2016-07-13 18:03:31 +0100

    first entry

---
+++ 8kyu_Grasshopper_Make_change.js
@@ -0,0 +1,7 @@
+//Challenge: https://www.codewars.com/kata/560dab9f8b50f89fd6000070
+
+var money = 10.00
+var candy = 1.42
+var chips = 2.40
+var soda = 1.00
+var change = (money) - ( candy + chips + soda )
