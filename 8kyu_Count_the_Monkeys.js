ec48b3c09abb2ada0b8b4ae37fd2848abb00fca4
Pete <petergarvin1@gmail.com>
2016-07-17 11:35:00 +0100

    first entry

---
+++ 8kyu_Count_the_Monkeys.js
@@ -0,0 +1,9 @@
+//Challenge: https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+function monkeyCount(n) {
+  var count = [];
+  for (var i = 1; i <= n; i++) {
+    count.push(i);
+  }
+  return count;
+}
