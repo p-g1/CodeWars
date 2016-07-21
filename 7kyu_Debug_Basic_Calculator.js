7fd1196e44a81f448a95dc29b373edffb4ce8eae
Pete <petergarvin1@gmail.com>
2016-07-21 11:55:56 +0100

    first entry

---
+++ 7kyu_Debug_Basic_Calculator.js
@@ -0,0 +1,19 @@
+//Challenge: https://www.codewars.com/kata/56368f37d464c0a43c00007f
+
+var calculate = function calculate(a, o, b) {
+ if(o === "+") {
+   return a + b;
+ }
+ else if(o === "-") {
+   return a - b;
+ }
+ else if(o === "/") {
+   return a / b;
+ }
+ else if(o === "*") {
+   return a * b;
+ }
+ else {
+   return null;
+ }
+}
