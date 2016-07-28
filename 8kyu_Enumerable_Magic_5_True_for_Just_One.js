87a27ca786bab039f58a45cf40fe84a498f20243
Pete <petergarvin1@gmail.com>
2016-07-28 22:26:23 +0100

    first entry

---
+++ 8kyu_Enumerable_Magic_5_True_for_Just_One.js
@@ -0,0 +1,16 @@
+// Challenge: https://www.codewars.com/kata/54599705cbae2aa60b0011a4
+
+function one(arr, fun){
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (fun(arr[i]) === true) {
+      count += 1;
+      }
+    }
+  if (count === 1) {
+    return true;
+    }
+  else {
+    return false;
+    }
+}
