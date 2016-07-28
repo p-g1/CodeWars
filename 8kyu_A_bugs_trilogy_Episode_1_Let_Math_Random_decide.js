a81bcd20d787f1bc6d2c4b3c36a5a37fc31733e4
Pete <petergarvin1@gmail.com>
2016-07-28 22:30:07 +0100

    first entry

---
+++ 8kyu_A_bugs_trilogy_Episode_1_Let_Math_Random_decide.js
@@ -0,0 +1,13 @@
+// Challenge https://www.codewars.com/kata/562e98755e9214cd2500003d
+
+var yourFutureCareer = function() {
+        var career =  Math.random();
+        if (career <= 0.32) {
+            return  "FrontEnd Developer";
+        }
+        else if (career <= 0.65) {
+            return "BackEnd Developer";
+        } else {
+            return "Full-Stack Developer";
+        }
+    }
