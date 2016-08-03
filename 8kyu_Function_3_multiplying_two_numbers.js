ad10194f41d3d39f629c209ac4b974efe0d560ac
Pete <petergarvin1@gmail.com>
2016-08-03 20:14:07 +0100

    first entry

---
+++ 8kyu_Function_3_multiplying_two_numbers.js
@@ -0,0 +1,5 @@
+// Challenge: https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+function multiply( a, b ){
+  return a * b;
+}
---
+++ 8kyu_Function_3_multiplying_two_numbers.rb
@@ -0,0 +1,5 @@
+# Challenge: https://www.codewars.com/kata/function-3-multiplying-two-numbers/ruby
+
+def multiply(x,z)
+  return x * z
+end
