dfde9ba00fa68fe69b5200651a1c25fd5777e73e
Pete <petergarvin1@gmail.com>
2016-08-03 20:09:30 +0100

    first entry

---
+++ 8kyu_How_many_lightsabers_do_you_own.js
@@ -0,0 +1,9 @@
+// Challenge: https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+
+function howManyLightsabersDoYouOwn(name) {
+  if (name === "Zach") {
+    return 18;
+  } else {
+    return 0;
+  }
+}
