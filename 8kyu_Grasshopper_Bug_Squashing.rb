af6f01b429f3738323812e8e6462fae6a8c963ac
Pete <petergarvin1@gmail.com>
2016-08-03 20:25:07 +0100

    first entry

---
+++ 8kyu_Grasshopper_Bug_Squashing.rb
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/grasshopper-bug-squashing/ruby
+
+health = 100
+position = 0
+coins = 0
+
+def main ()
+  roll_dice()
+  move()
+  combat()
+  get_coins()
+  buy_health()
+  print_status()
+end
