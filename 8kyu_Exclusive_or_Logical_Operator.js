e946ae3d67f4c64e00a28da650afe885b1879a5b
Pete <petergarvin1@gmail.com>
2016-08-31 22:13:17 +0100

    first entry

---
+++ 8kyu_Exclusive_or_Logical_Operator.js
@@ -0,0 +1,13 @@
+// Challenge: https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c
+
+function xor(a, b) {
+  if (a === true && b === false) {
+    return true;
+    }
+  else if (a === false && b === true){
+  return true;
+  }
+  else {
+    return false;
+    }
+}
