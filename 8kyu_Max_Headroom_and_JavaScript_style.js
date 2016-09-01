22b0745e838e2d193e77e8b4f80940a650f9e117
Pete <petergarvin1@gmail.com>
2016-09-01 19:23:34 +0100

    first entry

---
+++ 8kyu_Max_Headroom_and_JavaScript_style.js
@@ -0,0 +1,19 @@
+// Challenge: https://www.codewars.com/kata/52a47dd7e950edabfa000391
+
+function getMax1()
+{
+  var max =
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+  var max =
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
