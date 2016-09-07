43f66ebba1507d9bd0a9511031dbcc1a8fee91ac
Pete <petergarvin1@gmail.com>
2016-09-07 22:27:01 +0100

    second entry

--- Beta_Sum_of_Array_Averages.js
+++ Beta_Sum_of_Array_Averages.js
@@ -1,23 +1,15 @@
 // Challange: https://www.codewars.com/kata/56d5166ec87df55dbe000063
 
 function sumAverage(arr) {
-console.log(arr);
-  var result = [];
   var total = 0;
-  var length = 0;
-  var finalResult = 0;
+  var final = 0;
 
   for (var i = 0; i < arr.length; i++) {
-    length = arr[i].length;
-    total = 0;
     for (var j = 0; j < arr[i].length; j++) {
       total += arr[i][j];
       }
-      result.push(total/length);
+      final += total/arr[i].length;
+      total = 0;
       }
-    for (var x = 0; x < result.length; x++) {
-      finalResult += result[x];
-      }
-  return Math.floor(finalResult);
-
+   return Math.floor(final);
 }
