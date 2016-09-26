b421973646d49e3ab06b3ae4859095980b4ce864
Pete <petergarvin1@gmail.com>
2016-09-26 18:06:58 +0100

    first entry

---
+++ Beta_Keep_up_the_hoop.js
@@ -0,0 +1,6 @@
+// Challenge: https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+
+function hoopCount (n) {
+ if (n >= 10) { return("Great, now move on to tricks") }
+    else { return("Keep at it until you get it") }
+}
